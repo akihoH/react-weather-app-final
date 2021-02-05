bf38b31192ef6a49e6086e08ffd72207af2d2a63
Akiho.H <68869254+akh-io@users.noreply.github.com>
2021-02-05 18:58:08 +0100

    added a link to Github

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="App">
       Hello
+      <footer>
+      <a href="https://github.com/akh-io/react-weather-app-final" target="blank">
+        Open source
+      </a> 
+        {""} project coded by Akiho 
+      </footer>
+      
     </div>
   );
 }
