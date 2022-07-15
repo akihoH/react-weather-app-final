d55002820c4125126d4b139c588940ce0d46f7e9
Akiho Hamamoto <ahamamoto1125@gmail.com>
2022-07-15 22:08:48 +0200

    removed the date information

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-export default function FormattedDate (props) {
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let day = days[props.date.getDay()];
-let hours = props.date.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = props.date.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-return (
-<div className="formatted-date">
-  As of {day} {hours}:{minutes} CET
-</div>
-);
-}
+// export default function FormattedDate (props) {
+// let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// let day = days[props.date.getDay()];
+// let hours = props.date.getHours();
+// if (hours < 10) {
+//   hours = `0${hours}`;
+// }
+// let minutes = props.date.getMinutes();
+// if (minutes < 10) {
+//   minutes = `0${minutes}`;
+// }
+// return (
+// <div className="formatted-date">
+//   As of {day} {hours}:{minutes} CET
+// </div>
+// );
+// }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import FormattedDate from "./FormattedDate"
+// import FormattedDate from "./FormattedDate"
 import WeatherIcon from "./WeatherIcon"
 import WeatherTemperature from "./WeatherTemperature"
 
@@ -8,12 +8,12 @@ export default function Weatherinfo (props) {
 return (
   <div className="weatherInfo">
       <h1>
-        {props.data.city} 
+        {props.data.city}
       </h1>
-      <h2><FormattedDate date={props.data.date} /></h2>
+      {/* <h2><FormattedDate date={props.data.date} /></h2> */}
       <div className="row">
         <div className="col-6">
-          <WeatherIcon code={props.data.icon} alt={props.data.description}/> 
+          <WeatherIcon code={props.data.icon} alt={props.data.description}/>
             <ul className="main">
               <li className="text-capitalize">{props.data.description}</li>
               <li><WeatherTemperature celsius={props.data.temperature}/>
@@ -25,10 +25,10 @@ return (
             <li>Lowest Temp: {Math.round(props.data.minTemperature)}°C</li>
             <li>Higest Temp: {Math.round(props.data.maxTemperature)}°C</li>
             <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind Spped: {Math.round(props.data.windSpeed*10)/10}km/h</li>  
+            <li>Wind Spped: {Math.round(props.data.windSpeed*10)/10}km/h</li>
           </ul>
         </div>
-      </div>  
+      </div>
     </div>
 );
 }
