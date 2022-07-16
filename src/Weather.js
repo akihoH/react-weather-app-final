75a65e1d219c11bc2a6a42dcc47e0d282111d9a7
Akiho Hamamoto <ahamamoto1125@gmail.com>
2022-07-16 22:21:38 +0200

    updated the favicon and form format

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo"
 import WeatherForecast from "./WeatherForecast"
 import axios from "axios";
-import "./Weather.css"; 
+import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -43,19 +43,19 @@ export default function Weather(props) {
   if (weatherData.ready) {
     return (
     <div className="weather">
-      <form className="search-engine" onSubmit={handleSubmit}>  
+      <form className="search-engine" onSubmit={handleSubmit}>
         <div className="row">
-          <div className="col-9">
+          <div className="col-8">
             <input type="search" placeholder="Enter a city" className="form-control" autoFocus="on" onChange={handleCityChange}/>
           </div>
-          <div className="col-3">
+          <div className="col-4">
             <input type="submit" value="Search" className="btn btn-primary" />
           </div>
         </div>
       </form>
       <WeatherInfo data={weatherData}/>
       <WeatherForecast city={weatherData.city}/>
-     </div>  
+     </div>
     );
   } else {
   search()
